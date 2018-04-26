f62c4bd07cd7ecb902444d9cd8bbd17ad9412e3f
Bartbulteau <barthelemybulteau@gmail.com>
2018-04-26 14:04:28 +0200

    Fixing network error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var cors = require('cors');
 
 
 // set-up database
@@ -32,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // routes for api
 app.use('/api', apiRouter);
